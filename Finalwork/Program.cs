479cad9f5302e0282d9b9de55ee067ccbe0920ea
Marina Yarosh <yaroshmv@yandex.ru>
2024-04-23 05:41:21 +0300

    add code

--- Finalwork/Program.cs
+++ Finalwork/Program.cs
@@ -1,2 +1,62 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+string[] workArray = FillArray();
+string[] resultArray = GenerateNewArray(workArray);
+string firstArray = PrintArray(workArray);
+string secondArray = PrintArray(resultArray);
+Console.WriteLine(firstArray + " -> " + secondArray);
+
+string[] FillArray()
+{
+    Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
+    string? enterSymbols = Console.ReadLine();
+    if (enterSymbols == null) { enterSymbols = ""; };
+    char[] separators = new char[] { ',', ' ' };
+    string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return workArray;
+}
+
+string PrintArray(string[] workArray)
+{
+    string stringArray = "[";
+    for (int i = 0; i < workArray.Length; i++)
+    {
+        if (i == workArray.Length - 1)
+        {
+            stringArray += $"\"{workArray[i]}\"";
+            break;
+        }
+        stringArray += ($"\"{workArray[i]}\", ");
+    }
+    stringArray += "]";
+    return stringArray;
+}
+
+int CountStringSymbols(string[] workArray)
+{
+    int counter = 0;
+    foreach (string item in workArray)
+    {
+        if (item.Length <= 3)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+string[] GenerateNewArray(string[] workArray)
+{
+    int resultArrayLength = CountStringSymbols(workArray);
+    string[] resultArray = new string[resultArrayLength];
+    int i = 0;
+    foreach (string item in workArray)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[i] = item;
+            i++;
+        }
+    }
+    return resultArray;
+}
